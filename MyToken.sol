85d39d11157947e888494d726e45195648e495c8
Suhani-1211 <129710778+Suhani-1211@users.noreply.github.com>
2024-06-26 07:39:55 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,35 +1,31 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
+    constructor()
         ERC20("MyToken", "MTK")
-        Ownable(initialOwner)
         ERC20Permit("MyToken")
+        Ownable(msg.sender)
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-}
-contract TokenManager {
-    MyToken public token;
-
-    constructor(MyToken _token) {
-        token = _token;
-    }
 
     function burnTokens(uint256 amount) public {
-        token.burn(amount);
+        burn(amount);
+    }
+
+    function burnTokensFrom(address from, uint256 amount) public {
+        burnFrom(from, amount);
     }
 
     function transferTokens(address to, uint256 amount) public {
-        token.transfer(to, amount);
+        transfer(to, amount);
     }
 }
